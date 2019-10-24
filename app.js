9230a336dca5d2e3385b8832058ca80c99b12a29
Anant Mathur <anant.mathur007@gmail.com>
2019-10-24 19:16:22 +0530

    Connect Database

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+// MongoDB Setup
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then( () => console.log("Database Connected..."));
+
+
+// Routes
 app.get('/', (req, res) => {
     res.send("Hello from node");
 });
 
+// Listening to the port
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
