a698a70e04b970652483e08ba4bf506694de5a73
Anant Mathur <anant.mathur007@gmail.com>
2019-10-27 19:22:58 +0530

    Product validation  and product by id middleware

--- controllers/product.js
+++ controllers/product.js
@@ -9,10 +9,30 @@ const Product = require('../models/product');
 // Handle database error
 const {errorHandler} = require('../helpers/dbErrorHandler');
 
+
+// Find product by id
+exports.productById = async(req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if(err || !product)
+        {
+            return res.status(400).json({msg: 'Product not found'});
+        }
+
+        req.product = product;
+        next();
+    });
+};
+
+// Get a single product
+exports.read = async(req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+}
+
 // Create a new product
 exports.create = async(req, res) => {
     
-    // Formidable is used to handle form data. we are using it to hanf=dle image upload
+    // Formidable is used to handle form data. we are using it to handle image upload
     let form = new formidable.IncomingForm();
     form.keepExtensions = true // Extension for images
 
@@ -22,12 +42,29 @@ exports.create = async(req, res) => {
             return res.status(400).json({msg: 'Image could not be uploaded'});
         }
 
+        // check for all fields
+        const {name, description, price, quantity, category, shipping} = fields;
+        if(!name || !description || !price || !quantity || !category || !shipping)
+        {
+            return res.status(400).json({
+                err: "All fields are required"
+            })
+        }
+
         // Create new product now
         let product = new Product(fields);
 
         // Handle files
         if(files.photo)
         {
+            // Validate file size less than 1 MB
+            if(files.photo.size > 1000000)
+            {
+                return res.status(400).json({
+                    msg: "File size should be less than 1 mb"
+                })
+            }
+
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
--- routes/product.js
+++ routes/product.js
@@ -6,12 +6,13 @@ const {requireSignin, isAdmin, isAuth} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
 // Require the actual controller
-const {create} = require('../controllers/product');
+const {create, productById, read} = require('../controllers/product');
 
 // Routes
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
-
+router.get('/product/:productId', read);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
