48ba7ec6bb2526fcb8534045e08154d67ddeae07
Anant Mathur <anant.mathur007@gmail.com>
2019-11-10 16:59:12 +0530

    Prepare for deployment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path'); 
 
 // Database
 const mongoose = require('mongoose');
@@ -44,6 +45,17 @@ app.use('/api',categoryRoutes);
 app.use('/api',productRoutes);
 app.use('/api', braintreeRoutes);
 app.use('/api', orderRoutes);
+
+// Serve Static Assets in production
+if(process.env.NODE_ENV === 'production')
+{
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 
 // Listening to the port
 const port = process.env.PORT || 5000;
