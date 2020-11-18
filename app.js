90b0c664ae74e05759a6c27b17f15b9a5c9b9128
Anant <anant.mathur007@gmail.com>
2020-11-18 22:59:34 +0530

    change ports and prepare to deploy on oracle

--- app.js
+++ app.js
@@ -1,65 +1,65 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path'); 
+const path = require("path");
 
 // Database
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Middleware utilities
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const expressValidator = require('express-validator');
-const cors = require('cors');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const cors = require("cors");
 
 // Config Variables
-require('dotenv').config();
+require("dotenv").config();
 
 // Import Routes
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const braintreeRoutes = require('./routes/braintree');
-const orderRoutes = require('./routes/order');
-
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const braintreeRoutes = require("./routes/braintree");
+const orderRoutes = require("./routes/order");
 
 // MongoDB Setup
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
-}).then( () => console.log("Database Connected..."));
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database Connected..."));
 
 // Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
 
 // Routes middleware
-app.use('/api',authRoutes);
-app.use('/api',userRoutes);
-app.use('/api',categoryRoutes);
-app.use('/api',productRoutes);
-app.use('/api', braintreeRoutes);
-app.use('/api', orderRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", braintreeRoutes);
+app.use("/api", orderRoutes);
 
 // Serve Static Assets in production
-if(process.env.NODE_ENV === 'production')
-{
-    // Set static folder
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // Listening to the port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
